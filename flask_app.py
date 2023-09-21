dd0b266f3607e291fae97e1ef00cf0f2c8ddbf23
Daniel-Nkunga <144631064+Daniel-Nkunga@users.noreply.github.com>
2023-09-21 08:14:17 -0500

    Creating assignment folders

--- flask_app.py
+++ flask_app.py
@@ -2,7 +2,7 @@
 from flask import *
 
 app = Flask(__name__)
-app.secret_key = '88'
+app.secret_key = 'wowcool88'
 
 @app.route('/')
 def serve_home():
@@ -40,44 +40,66 @@ def serve_good():
 def serve_valid():
     return send_from_directory('static', 'HelloThere.html')
 
-questions = [
-    'question1.html',
-    'question2.html',
-    'question3.html',
-    'question4.html',
-    'question5.html',
-    'question6.html',
-    'question7.html',
-    'question8.html',
-    'question9.html',
-]
+# @app.route('/quiz', methods=['GET'])
+# def quiz():
+#     num = request.args.get("quiz", None)
+#     if num is None:
+#         return redirect(url_for("quiz", quiz=1))
+#     num = int(num)
+#     if num == 1:
+#         return send_from_directory('static', 'question1.html')
+#     elif num == 2:
+#         return send_from_directory('static', 'question2.html')
+#     elif num == 3:
+#         return send_from_directory('static', 'question3.html')
+#     elif num == 4:
+#         return send_from_directory('static', 'question4.html')
+#     elif num == 5:
+#         return send_from_directory('static', 'question5.html')
+#     elif num == 6:
+#         return send_from_directory('static', 'question6.html')
+#     elif num == 7:
+#         return send_from_directory('static', 'question7.html')
+#     elif num == 8:
+#         return send_from_directory('static', 'question8.html')
+#     elif num == 9:
+#         return send_from_directory('static', 'question9.html')
+#     else: 
+#         return "Invalid quiz question number."
 
 @app.route('/quiz', methods=['GET'])
-def quiz():
-    num = request.args.get("quiz", None)
-    if num is None:
-        return redirect(url_for("quiz", quiz=1))
-    num = int(num)
-    if num == 1:
-        return send_from_directory('static', 'question1.html')
-    elif num == 2:
-        return send_from_directory('static', 'question2.html')
-    elif num == 3:
-        return send_from_directory('static', 'question3.html')
-    elif num == 4:
-        return send_from_directory('static', 'question4.html')
-    elif num == 5:
-        return send_from_directory('static', 'question5.html')
-    elif num == 6:
-        return send_from_directory('static', 'question6.html')
-    elif num == 7:
-        return send_from_directory('static', 'question7.html')
-    elif num == 8:
-        return send_from_directory('static', 'question8.html')
-    elif num == 9:
-        return send_from_directory('static', 'question9.html')
-    else: 
-        return "Invalid quiz question number."
+def generate_form():
+    previtems = {
+        'item1': 'value1',
+        'item2': 'value2',
+        'quiz_question': 'What is your name?',
+    }
+
+    # Render a template containing your code
+    return render_template('form_template.html', previtems=previtems)
+
+@app.route('/example')
+def serve_example():
+    return send_from_directory('static', 'JScriptExample.html')
+
+#Segment to be Quiz 1, Quiz 2, etc. 
+
+#SEWARD THINGS BELOW
+
+# @app.route('/quiz')
+# def quiz():
+#     if request.method == "GET" and "quiz" in request.args:
+#         num = int(request.args["quiz"])
+#         return render_template(f"question{num+1}.html", previousitems=request.args)
+#     return render_template("question1.html")
+
+# @app.route('/quiz')
+# def quiz():
+#     quiz={"title":"Ceaning Implement","1":{"Option1"}}
+#     if request.method == "GET" and "quiz" in request.args:
+#         num = int(request.args["quiz"])
+#         return render_template(f"question.html", previousitems=request.args)
+#     return render_template("question.html", question=question)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=4208)
