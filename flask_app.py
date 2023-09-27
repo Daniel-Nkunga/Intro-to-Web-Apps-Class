be6e6dc0f26e7f5c6c57a0a192f51854e2e98a88
Daniel-Nkunga <144631064+Daniel-Nkunga@users.noreply.github.com>
2023-09-27 08:37:41 -0500

    Using ChatGPT to update question path

--- flask_app.py
+++ flask_app.py
@@ -40,73 +40,21 @@ def serve_good():
 def serve_valid():
     return send_from_directory('static', 'HelloThere.html')
 
-# @app.route('/quiz', methods=['GET'])
-# def quiz():
-#     num = request.args.get("quiz", None)
-#     if num is None:
-#         return redirect(url_for("quiz", quiz=1))
-#     num = int(num)
-#     if num == 1:
-#         return send_from_directory('static', 'question1.html')
-#     elif num == 2:
-#         return send_from_directory('static', 'question2.html')
-#     elif num == 3:
-#         return send_from_directory('static', 'question3.html')
-#     elif num == 4:
-#         return send_from_directory('static', 'question4.html')
-#     elif num == 5:
-#         return send_from_directory('static', 'question5.html')
-#     elif num == 6:
-#         return send_from_directory('static', 'question6.html')
-#     elif num == 7:
-#         return send_from_directory('static', 'question7.html')
-#     elif num == 8:
-#         return send_from_directory('static', 'question8.html')
-#     elif num == 9:
-#         return send_from_directory('static', 'question9.html')
-#     else: 
-#         return "Invalid quiz question number."
+@app.route('/quiz', methods=['GET'])
+def quiz():
+    # Get the question number from the URL query parameter
+    question_number = request.args.get('question_number', type=int)
+    
+    # Load the corresponding question HTML file
+    question_filename = f'questions/question{question_number}.html'
 
-# @app.route('/quiz', methods=['GET'])
-# def generate_form():
-#     previtems = {
-#         'item1': 'value1',
-#         'item2': 'value2',
-#         'quiz_question': 'What is your name?',
-#     }
+    try:
+        with open(question_filename, 'r') as question_file:
+            question_content = question_file.read()
+    except FileNotFoundError:
+        return "Question not found"
 
-#     # Render a template containing your code
-#     return render_template('form_template.html', previtems=previtems)
-
-# @app.route('/example')
-# def serve_example():
-#     return send_from_directory('static', 'JScriptExample.html')
-
-#Segment to be Quiz 1, Quiz 2, etc. 
-
-#SEWARD THINGS BELOW
-
-# @app.route('/quiz')
-# def quiz():
-#     if request.method == "GET" and "quiz" in request.args:
-#         num = int(request.args["quiz"])
-#         return render_template(f"question{num+1}.html", previousitems=request.args)
-#     return render_template("question1.html")
-
-# @app.route('/quiz')
-# def quiz():
-#     quiz={"title":"Ceaning Implement","1":{"Option1"}}
-#     if request.method == "GET" and "quiz" in request.args:
-#         num = int(request.args["quiz"])
-#         return render_template(f"question.html", previousitems=request.args)
-#     return render_template("question.html", question=question)
-
-@app.route('/quiz', methods=['GET', 'POST'])
-def wtf_quiz():
-    form = PopQuiz()
-    if form.validate_on_submit():
-        return redirected(url_for('passed')) return
-    render_template('quiz.html', form=form)
+    return render_template('quiz.html', question_content=question_content)
 
 @app.route('/passed')
 def passed():
