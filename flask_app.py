fd506e0218f4b38634d8d92c47bebb40a56c42a8
Daniel-Nkunga <144631064+Daniel-Nkunga@users.noreply.github.com>
2023-10-01 23:29:11 -0500

    Flask app tinkering

--- flask_app.py
+++ flask_app.py
@@ -40,25 +40,29 @@ def serve_good():
 def serve_valid():
     return send_from_directory('static', 'HelloThere.html')
 
-@app.route('/quiz', methods=['GET'])
-def quiz():
-    # Get the question number from the URL query parameter
-    question_number = request.args.get('question_number', type=int)
+# @app.route('/quiz', methods=['GET'])
+# def quiz():
+#     # Get the question number from the URL query parameter
+#     question_number = request.args.get('question_number', type=int)
     
-    # Load the corresponding question HTML file
-    question_filename = f'questions/question{question_number}.html'
+#     # Load the corresponding question HTML file
+#     question_filename = f'questions/question{question_number}.html'
 
-    try:
-        with open(question_filename, 'r') as question_file:
-            question_content = question_file.read()
-    except FileNotFoundError:
-        return "Question not found"
+#     try:
+#         with open(question_filename, 'r') as question_file:
+#             question_content = question_file.read()
+#     except FileNotFoundError:
+#         return "Question not found"
 
-    return render_template('quiz.html', question_content=question_content)
+#     return render_template('quiz.html', question_content=question_content)
 
 @app.route('/passed')
 def passed():
     return render_template('passed.html')
+
+@app.route('/game')
+def serve_valid():
+    return send_from_directory('static\Game', 'game.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=4208)
