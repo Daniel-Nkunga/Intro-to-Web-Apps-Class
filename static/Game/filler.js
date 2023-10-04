35fe546839f07b5a147460b9dcdb4c345bb3b645
Daniel-Nkunga <144631064+Daniel-Nkunga@users.noreply.github.com>
2023-10-03 23:35:27 -0500

    Places buttons on screen

--- static/Game/filler.js
+++ static/Game/filler.js
@@ -5,14 +5,25 @@ function setup() {
             const cell = document.createElement('div');
             cell.classList.add('cell');
             cell.id = `cell_${i}_${j}`;
-            cell.addEventListener('click', () => {
-                const startCell = document.getElementById(`cell_${i}_${j}`);
-                highlight(startCell);
-                repaint(); 
-            });
+            // cell.addEventListener('click', () => {
+            //     const clickedCell = document.getElementById(`cell_${i}_${j}`);
+            //     highlight(clickedCell);
+            // });
             board.appendChild(cell);
         }
     }
+
+    // document.getElementById("botton1").addEventListener("click", floodFill(grid, 0, 0, 'cell_${i}_${j}', 1));
+
+    const colorButtons = document.querySelectorAll('.button');
+    colorButtons.forEach((button) => {
+        button.addEventListener('click', () => {
+            const color = button.getAttribute('data-color');
+            const targetColorCell = document.getElementById('cell_0_0');
+            const targetColor = parseInt(targetColorCell.textContent);
+            floodFill(grid, 0, 0, targetColor, parseInt(color));
+        });
+    });
     randomize();
     repaint();
 }
@@ -26,16 +37,11 @@ function randomize() {
             let randomNumber;
             do {
                 randomNumber = Math.floor(Math.random() * 6) + 1;
-            } while ((i > 0 && grid[i - 1][j] === randomNumber) || (j > 0 && grid[i][j - 1] === randomNumber) || (j > 0 && i > 0 && grid[i - 1][j - 1] === randomNumber));         
+            } while ((i > 0 && grid[i - 1][j] === randomNumber) || (j > 0 && grid[i][j - 1] === randomNumber) || (j > 0 && i > 0 && grid[i - 1][j - 1] === randomNumber));
             grid[i][j] = randomNumber;
             cell.textContent = randomNumber;
         }
     }
-}
-
-function alertCell(cell) {
-    const cellValue = cell.textContent;
-    alert(`Cell value: ${cellValue}`);
 }
 
 function repaint() {
@@ -44,30 +50,31 @@ function repaint() {
             const cell = document.getElementById(`cell_${i}_${j}`);
             const cellValue = parseInt(cell.textContent);
             let color;
+
             switch (cellValue) {
                 case 1:
-                    color = "red";
+                    color = getComputedStyle(document.documentElement).getPropertyValue('--color1');
                     break;
                 case 2:
-                    color = "orange";
+                    color = getComputedStyle(document.documentElement).getPropertyValue('--color2');
                     break;
                 case 3:
-                    color = "yellow";
+                    color = getComputedStyle(document.documentElement).getPropertyValue('--color3');
                     break;
                 case 4:
-                    color = "green";
+                    color = getComputedStyle(document.documentElement).getPropertyValue('--color4');
                     break;
                 case 5:
-                    color = "blue";
+                    color = getComputedStyle(document.documentElement).getPropertyValue('--color5');
                     break;
                 case 6:
-                    color = "purple";
+                    color = getComputedStyle(document.documentElement).getPropertyValue('--color6');
                     break;
                 case 0:
-                    color = "black";
+                    color = getComputedStyle(document.documentElement).getPropertyValue('--color0');
                     break;
                 default:
-                    color = "white";
+                    color = getComputedStyle(document.documentElement).getPropertyValue('--colorNone');
                     break;
             }
             cell.style.backgroundColor = color;
@@ -75,34 +82,24 @@ function repaint() {
     }
 }
 
-function highlight(cell){
-    const cellValue = cell.textContent;
-    cellValue = 0;
-    alertCell(cell)
-}
-
-function game_action(cell){
-    const cellValue = cell.textContent;
-    flood_fill(board, 0, 0, board[0][0], cellValue)
+function highlight(cell) {
+    const cellValue = parseInt(cell.textContent);
+    if (cellValue !== 0) {
+        cell.textContent = '0';
+    }
     repaint();
 }
 
-function flood_fill(grid, x, y, target, replacement){
-    if(grid[x][y] != target){
-        return;
-    }  
-    grid[x][y] = replacement_color;
-
-   if (x > 0){
-    flood_fill(grid, x -1, y, target, replacement)
-   }
-   if (x < 10){
-    flood_fill(grid, x + 1, y, target, replacement)
-   }
-   if (y > 0){
-    flood_fill(grid, x, y - 1, target, replacement)
-   }
-   if(y < 0 ){
-    flood_fill(grid, x, y + 1, target, replacement)
-   }
-} 
+function floodFill(matrix, x, y, targetColor, replacementColor) {
+    if (x < 0 || x >= matrix.length || y < 0 || y >= matrix[0].length) {
+      return;
+    }
+    if (matrix[x][y] !== targetColor) {
+      return;
+    }
+    matrix[x][y] = replacementColor;
+    floodFill(matrix, x + 1, y, targetColor, replacementColor); 
+    floodFill(matrix, x - 1, y, targetColor, replacementColor); 
+    floodFill(matrix, x, y + 1, targetColor, replacementColor); 
+    floodFill(matrix, x, y - 1, targetColor, replacementColor); 
+  }
