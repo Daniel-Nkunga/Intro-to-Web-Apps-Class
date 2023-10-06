9d3a174989d664a24e722b0bc32e68021c6ddb03
Daniel-Nkunga <144631064+Daniel-Nkunga@users.noreply.github.com>
2023-10-06 10:08:20 -0500

    Working on getting the final page to work

--- flask_app.py
+++ flask_app.py
@@ -41,9 +41,11 @@ def serve_good():
 def serve_valid():
     return send_from_directory('static', 'HelloThere.html')
 
+mascots=["elmo", "barney", "mario"]
+
 @app.route('/quiz')
 def quiz():
-    if request.method == "GET" and "form" in request.args:
+    if request.method == "GET" and "quiz" in request.args:
         num = int(request.args['quiz'])
         print(num)
         if num == 1:
@@ -65,16 +67,16 @@ def quiz():
         elif num == 9:
             session["LoL"]=request.args["LoL"]
 
-            # # Generate a random seed based on the user's answers
-            # seed = hash((session["name"], session["utensil"], session["superpower"], session["spirit"], session["smell"], session["dance"], session["transport"]))
-            # random.seed(seed)
+            # Generate a random seed based on the user's answers
+            seed = hash((session["device"], session["amount"], session["price"], session["color"], session["multiplayer"], session["frequency"], session["victory"], session["completion"], session["LoL"]))
+            random.seed(seed)
 
-            # # Randomly select a mascot for this session
-            # session["mascot"] = random.choice(mascots)
+            # Randomly select a mascot for this session
+            session["mascot"] = random.choice(mascots)
 
-        #     session["image"] = f"static/images/{session['mascot'].lower().replace(' ','')}.webp"
-        #     return render_template('finalmsg.html', session)
-        # return render_template(f'form{num+1}.html',request.args)
+            session["image"] = f"static/images/{session['mascot'].lower().replace(' ','')}.webp"
+            return render_template('finalmsg.html', session)
+        return render_template(f'form{num+1}.html',**request.args)
     return render_template('form1.html')
 
 
