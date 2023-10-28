1e4f0143963af051a82ea9ecac9589bd7f53d3c8
Daniel-Nkunga <144631064+Daniel-Nkunga@users.noreply.github.com>
2023-10-27 20:26:33 -0500

    The game is fully working
    This is a reverse point

--- static/Game/fillerGame.js
+++ static/Game/fillerGame.js
@@ -1,4 +1,6 @@
 const gridContainer = document.getElementById('grid-container');
+const outputDiv = document.getElementById('output');
+const inputElement = document.getElementById('input');
 
 function createGridItem(value) {
     const gridItem = document.createElement('div');
@@ -11,19 +13,14 @@ const boardSize = 10;
 let board = [];
 let game = -1;
 let player1Start, player2Start;
+let currentPlayer = 0;
 
-function printBoard() {
-    let boardString = '\n';
-    for (let i = 0; i < boardSize; i++) {
-        for (let j = 0; j < boardSize; j++) {
-            boardString += board[i][j] + ' ';
-        }
-        boardString += '\n';
-    }
-    return boardString;
+function print(text) {
+    outputDiv.innerText = text;
 }
 
 function render() {
+    gridContainer.innerHTML = '';
     for (let i = 0; i < boardSize; i++) {
         for (let j = 0; j < boardSize; j++) {
             createGridItem(board[i][j]);
@@ -41,7 +38,6 @@ function startGame(selectedGame) {
         player2Start = Math.floor(boardSize / 2);
     }
 
-    // Initialize the game board
     board = Array.from({ length: boardSize }, () => Array(boardSize).fill(0));
     for (let i = 0; i < boardSize; i++) {
         for (let j = 0; j < boardSize; j++) {
@@ -58,8 +54,103 @@ function startGame(selectedGame) {
         }
     }
 
-    // Render the game board in the grid
     render();
+    print("Player 1's turn");
+    playGame();
 }
 
-startGame(0);  // Start the game
+function isValid(target) {
+    if (target < 1 || target > 6) {
+        return false;
+    }
+    if (game === 0) {
+        if (target === board[0][0] || target === board[boardSize - 1][boardSize - 1]) {
+            return false;
+        }
+    } else if (game === 1) {
+        if (target === board[player1Start][player1Start] || target === board[player2Start][player2Start]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function floodFill(row, col, targetColor, replacementColor) {
+    if (row < 0 || row >= boardSize || col < 0 || col >= boardSize) {
+        return;
+    }
+    if (board[row][col] !== targetColor) {
+        return;
+    }
+    board[row][col] = replacementColor;
+    floodFill(row - 1, col, targetColor, replacementColor);
+    floodFill(row + 1, col, targetColor, replacementColor);
+    floodFill(row, col - 1, targetColor, replacementColor);
+    floodFill(row, col + 1, targetColor, replacementColor);
+}
+
+function switchPlayer() {
+    if (currentPlayer === 1) {
+        currentPlayer = 0;
+    } else {
+        currentPlayer = 1;
+    }
+}
+
+function checkWin() {
+    const uniqueColors = new Set();
+    for (let i = 0; i < boardSize; i++) {
+        for (let j = 0; j < boardSize; j++) {
+            uniqueColors.add(board[i][j]);
+        }
+    }
+    if (uniqueColors.size > 2) {
+        return false;
+    } else {
+        let player1Count = board[player1Start][player1Start];
+        let player2Count = board[player2Start][player2Start];
+        for (let i = 0; i < boardSize; i++) {
+            for (let j = 0; j < boardSize; j++) {
+                if(board[i][j] == board[player1Start][player1Start]){
+                    player1Count++;
+                }else if(board[i][j] == board[player2Start][player2Start]){
+                    player2Count++;
+                } else {
+                    console.print();
+                }
+            }
+        }
+        if(player1Count > player2Count){
+            print(`Player 1: ${player1Count} Player 2: ${player2Count}\nPlayer 1 Wins!`);
+        } else if (player1Count < player2Count){
+            print(`Player 1: ${player1Count} Player 2: ${player2Count}\nPlayer 2 Wins!`);
+        } else {
+            print(`Player 1: ${player1Count} Player 2: ${player2Count}\nIt's a tie!`);
+        }
+        return true;
+    }
+}
+
+function print(text) {
+    outputDiv.innerHTML = `<p class="game-text">${text}</p>`;
+}
+
+function playGame() {
+    inputElement.addEventListener('keyup', function (event) {
+        if (event.key === 'Enter') {
+        const inputValue = parseInt(inputElement.value, 10);
+            if (isValid(inputValue)) {
+                if (currentPlayer === 0) {
+                    floodFill(player1Start, player1Start, board[player1Start][player1Start], inputValue);
+                } else {
+                    floodFill(player2Start, player2Start, board[player2Start][player2Start], inputValue);
+                }
+                render();
+                switchPlayer();
+                checkWin();
+            }
+            inputElement.value = '';
+        }
+    });
+}
+startGame(0);
